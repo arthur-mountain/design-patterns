13644d18743db071e278fef6e7e979590665cc8d
arthur <arthur.oceanus@gmail.com>
2022-04-22 22:38:34 +0800

    add factory pattern

--- README.md
+++ README.md
@@ -3,10 +3,14 @@ Front-End design patterns ( JS design patterns )
 
 ---
 ####  Retry Pattern
-    Immediately retry or Delay retry, something like api call.
+    Immediately retry or Delay retry.
+    Ex. something  like api call.
 
-#### Singleton
-    That a class has only a single instance.
-    That Global access point to that single instance.
-    The singleton object is initialized only when it’s requested for the first time.
-    Ex.A single database object shared by different parts of the program.
+####  Singleton Pattern
+    1. class 只會有一個 instance
+    2. class 只會在第一次呼叫時進行初始化
+    Ex. 那個唯一的 database instance 會在不同程式間來進行使用。
+
+####  Factory Pattern
+    1. 工廠模式是一種提供【通用的介面】來【委派物件實例化】。
+    常被使用在【同時管理與操作，多個有相似特性卻仍不同的物件集合】
---
+++ src/factory.ts
@@ -0,0 +1,73 @@
+/**
+ * @description 工廠模式是一種提供【通用的介面】來【委派物件實例化】
+ * 這個模式經常被使用在 【同時管理與操作，多個有相似特性卻仍不同的物件集合】。
+*/
+
+interface FactoryType {
+  fly: () => void;
+}
+
+class Factory {
+  createPlane: <T>(type: string) => FactoryType & T;
+
+  constructor() {
+    this.createPlane = function (type) {
+      let ball;
+
+      if (type === Airplane.type) {
+        ball = new Airplane();
+      } else if (type === JIT.type) {
+        ball = new JIT();
+      }
+
+      ball.fly = function () {
+        return `The ${this.name} is flying.`;
+      };
+
+      return ball;
+    };
+  }
+}
+
+class Airplane {
+  static type = 'airplane';
+  name: string;
+  speedDown: () => string;
+
+  constructor() {
+    this.name = Airplane.type;
+    this.speedDown = function () {
+      return `The ${this.name} is speedDown.`;
+    };
+  }
+}
+
+class JIT {
+  static type = 'JIT';
+  name: string;
+  speedUp: () => string;
+  fight: () => string;
+
+  constructor() {
+    this.name = JIT.type;
+    this.speedUp = () => {
+      return `The ${this.name} is speedUp.`;
+    };
+    this.fight = () => {
+      return `The ${this.name} is fighting.`;
+    }
+  }
+}
+
+const factory = new Factory();
+const myAirplane = factory.createPlane<Airplane>(Airplane.type);
+const myJIT = factory.createPlane<JIT>(JIT.type);
+
+// In the Factory create shared method or property to instance.
+console.log(myAirplane.fly());
+console.log(`${myJIT.fly()} \n`);
+
+// Each instance also have their own properties and method.
+console.log(`${myAirplane.speedDown()}\n`); // The airplane is speedDown.
+console.log(myJIT.speedUp()); // The JIT is speedUp.
+console.log(myJIT.fight()); // The JIT is also fighting.
